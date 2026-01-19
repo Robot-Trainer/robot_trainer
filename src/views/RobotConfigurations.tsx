437ba7ec9be980990d71a1a3a91dd4c2513a0bbb
Bernie Telles <btelles@gmail.com>
2026-01-19 11:59:05 -0800

    Remove unused RobotConfigurations properties.

--- src/views/RobotConfigurations.tsx
+++ src/views/RobotConfigurations.tsx
@@ -3,12 +3,6 @@ import ResourceManager from "../ui/ResourceManager";
 import { robotConfigurationsTable } from "../db/schema";
 import RobotConfigurationWizard from "./RobotConfigurationWizard";
 
-const RobotConfigurationFields = [
-  { name: "serialNumber", label: "Serial Number" },
-  { name: "name", label: "Name" },
-  { name: "notes", label: "Notes" },
-];
-
 const RobotsView: React.FC = () => {
   return (
     <div className="p-6">
