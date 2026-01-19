a3cfa849a6eee620ca5ac2de363de140c8411705
Bernie Telles <btelles@gmail.com>
2026-01-19 11:49:06 -0800

    Add modality field indicating sim or real to robots and cameras, and change the id field to accept non-default values.

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,23 +1,27 @@
-import { integer, pgTable, varchar, uuid, json, text, timestamp, primaryKey, jsonb } from "drizzle-orm/pg-core";
+import { integer, pgTable, varchar, uuid, json, text, timestamp, primaryKey, jsonb, pgEnum } from "drizzle-orm/pg-core";
+
+export const robotModalityEnum = pgEnum("robotModality", ["real", "simulated"]);
+export const cameraModalityEnum = pgEnum("cameraModality", ["real", "simulated"]);
 
 export const userConfigTable = pgTable("user_config", {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  id: integer().primaryKey().generatedByDefaultAsIdentity(),
   userId: uuid('user_id'),
   config: json().default({})
 });
 
 export const robotsTable = pgTable("robots", {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  id: integer().primaryKey().generatedByDefaultAsIdentity(),
   serialNumber: varchar('serial_number').default(''),
   name: varchar('name').default(''),
   model: varchar('model').default(''),
   notes: text('notes').default(''),
   data: json('data').default({}),
+  modality: robotModalityEnum('modality').default('real'),
   createdAt: timestamp("created_at", { withTimezone: true }).defaultNow(),
 });
 
 export const teleoperatorsTable = pgTable("teleoperators", {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  id: integer().primaryKey().generatedByDefaultAsIdentity(),
   serialNumber: varchar("serial_number").default(""),
   name: varchar("name").default(""),
   model: varchar("model").default(""),
@@ -28,7 +32,7 @@ export const teleoperatorsTable = pgTable("teleoperators", {
 
 
 export const robotConfigurationsTable = pgTable("robot_configurations", {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  id: integer().primaryKey().generatedByDefaultAsIdentity(),
   name: varchar("name").notNull(),
   notes: text("notes"),
   createdAt: timestamp("created_at", { withTimezone: true }).defaultNow(),
@@ -36,17 +40,18 @@ export const robotConfigurationsTable = pgTable("robot_configurations", {
 
 
 export const camerasTable = pgTable("cameras", {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  id: integer().primaryKey().generatedByDefaultAsIdentity(),
   serialNumber: varchar('serial_number').default(''),
   name: varchar('name').default(''),
   resolution: varchar('resolution').default(''),
   fps: integer('fps').default(0),
   data: json('data').default({}),
+  modality: cameraModalityEnum('modality').default('real'),
   createdAt: timestamp("created_at", { withTimezone: true }).defaultNow(),
 });
 
 export const robotModelsTable = pgTable("robot_models", {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  id: integer().primaryKey().generatedByDefaultAsIdentity(),
   name: varchar('name').notNull(),
   dirName: varchar('dir_name').notNull(),
   className: varchar('class_name').notNull(),
@@ -56,10 +61,10 @@ export const robotModelsTable = pgTable("robot_models", {
 });
 
 export const teleoperatorModelsTable = pgTable("teleoperator_models", {
-  id: varchar('id').primaryKey(),
-  className: varchar('class_name').notNull(),
-  configClassName: varchar('config_class_name').notNull(),
-  data: json('data').default({}),
+  id: integer().primaryKey().generatedByDefaultAsIdentity(),
+  className: varchar("class_name").notNull(),
+  configClassName: varchar("config_class_name").notNull(),
+  data: json("data").default({}),
   createdAt: timestamp("created_at", { withTimezone: true }).defaultNow(),
 });
 
@@ -81,7 +86,7 @@ export const configCamerasTable = pgTable("config_cameras", {
 
 export const configTeleoperatorsTable = pgTable("config_teleoperators", {
   configurationId: integer("configuration_id").references(() => robotConfigurationsTable.id).notNull(),
-  teleoperatorId: varchar("teleoperator_id").references(() => teleoperatorModelsTable.id, { onDelete: "set null" }).notNull(),
+  teleoperatorId: integer("teleoperator_id").references(() => teleoperatorModelsTable.id, { onDelete: "set null" }).notNull(),
   snapshot: jsonb("snapshot").notNull(),
 }, (t) => [
   primaryKey({ columns: [t.configurationId, t.teleoperatorId] }),
