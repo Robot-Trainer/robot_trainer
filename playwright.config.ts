25412d814cfc6901afa255cfbd3e3321b3a871fd
Bernie Telles <btelles@gmail.com>
2026-03-06 18:04:23 -0800

    Use multiple workers in playwright again.

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,10 +4,10 @@ export default defineConfig({
   // Look for test files in the "tests" directory, relative to this configuration file.
   testDir: "src/tests",
   globalSetup: "./src/tests/global-setup.ts",
-  workers: 1,
-  fullyParallel: false,
+  workers: process.env.CI ? 1 : undefined, // Run tests sequentially in CI to avoid potential issues with shared state
+  fullyParallel: true,
   testMatch: "src/tests/*.spec.ts",
-  timeout: 60000,
+  timeout: 30000,
   use: {
     video: "on",
   },
